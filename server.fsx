f8e8589afa89e59f96e9800e5fd5297b6a57e20a
tpetricek <tomas@tomasp.net>
2015-04-22 23:01:39 +0100

    Use net40 version of FCS

--- server.fsx
+++ server.fsx
@@ -1,7 +1,6 @@
-﻿#I "."
-#r "packages/FSharp.Data/lib/net40/FSharp.Data.dll"
+﻿#r "packages/FSharp.Data/lib/net40/FSharp.Data.dll"
 #r "packages/Suave/lib/net40/Suave.dll"
-#r "packages/FSharp.Compiler.Service/lib/net45/FSharp.Compiler.Service.dll"
+#r "packages/FSharp.Compiler.Service/lib/net40/FSharp.Compiler.Service.dll"
 
 open Microsoft.FSharp.Compiler.SourceCodeServices
 open Microsoft.FSharp.Compiler.Interactive.Shell
