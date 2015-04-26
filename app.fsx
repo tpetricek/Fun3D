7487e0a2b7d06d36065264de058dad6a8c1bb2ea
tpetricek <tomas@tomasp.net>
2015-04-26 16:13:43 +0100

    Republish

--- app.fsx
+++ app.fsx
@@ -73,8 +73,8 @@ let formatComment cmt (sb:StringBuilder) =
 let formatTipElement isSingle el (sb:StringBuilder) =
     match el with
     | FSharpToolTipElement.None -> ()
-    | FSharpToolTipElement.Single(it, comment) -> 
-        sb.AppendLine(sprintf "<p class=\"sig\">%s</p>" (htmlEncode it)) 
+    | FSharpToolTipElement.Single(it, comment) ->
+        sb.AppendLine(sprintf "<p class=\"sig\">%s</p>" (htmlEncode it))
         |> formatComment comment
     | FSharpToolTipElement.Group(items) ->
         let items, msg =
@@ -138,7 +138,7 @@ type FsiSession =
   { Session : FsiEvaluationSession
     ErrorString : StringBuilder }
 
-/// Start F# Interactive, reference all assemblies in `refFolder` 
+/// Start F# Interactive, reference all assemblies in `refFolder`
 /// evaluate the initial `loadScript` and return running 'FsiSession'
 let startSession refFolder loadScript =
   let sbOut = new StringBuilder()
@@ -170,18 +170,18 @@ let checkScriptStructure (scriptFile, source) = async {
   let! options = checker.GetProjectOptionsFromScript(scriptFile, source)
   let! parsed = checker.ParseFileInProject(scriptFile, source, options)
   match parsed.ParseTree with
-  | Some tree -> 
+  | Some tree ->
       match tree with
       // Expecting: single file containing single module named "Script"
       | ParsedInput.ImplFile
-          (ParsedImplFileInput(_,_,_,_,_,[SynModuleOrNamespace([id],_,decls,_,_,_,_)],_)) 
+          (ParsedImplFileInput(_,_,_,_,_,[SynModuleOrNamespace([id],_,decls,_,_,_,_)],_))
             when id.idText = "Script" ->
-        match decls with 
+        match decls with
         // Expecting: FunScript.Compiler.Compiler.Compile(<@ .. @>)
         // (if all user code is inside quotation, it does not get run)
         | [ SynModuleDecl.DoExpr
               (_, SynExpr.App
-                    ( _, _, SynExpr.LongIdent _, 
+                    ( _, _, SynExpr.LongIdent _,
                       SynExpr.Paren(SynExpr.Quote _, _, _, _), _), _) ] -> ()
         | _ -> failwith "Unexpected AST!"
       | _ -> failwith "Unexpected AST!"
@@ -190,11 +190,11 @@ let checkScriptStructure (scriptFile, source) = async {
 /// Pass the specified code to FunScript and return JavaScript that we'll
 /// send back to the client (so that they can run it themselves)
 let evalFunScript { Session = fsiSession; ErrorString = sbErr } (scriptFile, code) = async {
-  let allCode = 
+  let allCode =
     [ yield "FunScript.Compiler.Compiler.Compile(<@"
       for line in getLines code do yield "  " + line
       yield "  |> Fun3D.Fun.show"
-      yield "@>)" ] 
+      yield "@>)" ]
     |> String.concat "\n"
   do! checkScriptStructure (scriptFile, allCode)
 
@@ -202,7 +202,7 @@ let evalFunScript { Session = fsiSession; ErrorString = sbErr } (scriptFile, cod
     match fsiSession.EvalExpression(allCode) with
     | Some value -> return value.ReflectionValue.ToString()
     | None -> return failwith "Evaluation failed."
-  with e -> 
+  with e ->
     let errors = sbErr.ToString()
     sbErr.Clear() |> ignore
     return failwithf "Evaluation failed: %O\nReported errors:\n%s" e errors }
@@ -219,15 +219,15 @@ type SnippetInfo = JsonProvider<"""{"title":"h","author":"h","info":"h",
 
 /// Request type that we want to send to www.fssnip.net
 type FsSnipTypes = JsonProvider<"""{
-  "request": { "title": "Hello", "author": "Tomas Petricek", "description": "Hello world", 
-    "code": "Fun.cube", "tags": [ "test" ], "public": true, "link": "http://tomasp.net", 
+  "request": { "title": "Hello", "author": "Tomas Petricek", "description": "Hello world",
+    "code": "Fun.cube", "tags": [ "test" ], "public": true, "link": "http://tomasp.net",
     "nugetpkgs": ["na"], "source": "fun3d" } }""">
 
-let header = 
+let header =
   "// This snippet has been shared from Fun3D web site.\n" +
   "// You can run it in your web browser at: www.fun3d.net!\n"
 
-/// Sends the specified snippet ('SnippetInfo' format from the 
+/// Sends the specified snippet ('SnippetInfo' format from the
 /// client) to the API exposed by www.fssnip.net.
 let shareSnippet jsonText = async {
   let info = SnippetInfo.Parse(jsonText)
@@ -235,14 +235,14 @@ let shareSnippet jsonText = async {
     FsSnipTypes.Request
       ( info.Title, info.Author, info.Info, header + info.Code, [| "fun3d" |], info.Public,
         "http://www.fun3d.net", [| |], "fun3d")
-  let! res = 
+  let! res =
     Http.AsyncRequestString
-      ( "http://api.fssnip.net/1/snippet", httpMethod="PUT", 
+      ( "http://api.fssnip.net/1/snippet", httpMethod="PUT",
         body=HttpRequestBody.TextRequest (req.ToString()) )
   return res }
 
 // ------------------------------------------------------------------------------------------------
-// Suave.io web server 
+// Suave.io web server
 // ------------------------------------------------------------------------------------------------
 
 open System
@@ -271,13 +271,13 @@ let getRequestParams (ctx:HttpContext) =
   sr.ReadToEnd()
 
 // This script is implicitly inserted before every source code we get
-let loadScript = 
+let loadScript =
   [| "#r \"Fun3D.Library.dll\"\n"
      "open Fun3D\n" |]
-let loadScriptString = 
+let loadScriptString =
   String.Concat(loadScript)
 
-let noCacheSuccess res = 
+let noCacheSuccess res =
   Writers.setHeader "Cache-Control" "no-cache, no-store, must-revalidate"
   >>= Writers.setHeader "Pragma" "no-cache"
   >>= Writers.setHeader "Expires" "0"
@@ -286,7 +286,7 @@ let noCacheSuccess res =
 /// The main handler for Suave server!
 let serviceHandler fsi scriptFile : WebPart = fun ctx -> async {
   match ctx.request.url.LocalPath, getRequestParams ctx with
-  
+
   // Transform F# `source` into JavaScript and return it
   | "/run", (_, _, source) ->
       let! jscode = evalFunScript fsi (scriptFile, source)
@@ -309,16 +309,16 @@ let serviceHandler fsi scriptFile : WebPart = fun ctx -> async {
 
   // Get method overloads & parameter info at the specified location in the source
   | "/methods", (Some line, Some col, source) ->
-      let! meths = 
-        getMethodOverloads (scriptFile, loadScriptString + source) 
+      let! meths =
+        getMethodOverloads (scriptFile, loadScriptString + source)
                            (line + loadScript.Length, col)
       let res = JsonTypes.Methods(Array.ofSeq meths)
       return! ctx |> noCacheSuccess (res.JsonValue.ToString())
 
   // Get auto-completion for the specified location
   | "/declarations", (Some line, Some col, source) ->
-      let! decls = 
-        getDeclarations (scriptFile, loadScriptString + source) 
+      let! decls =
+        getDeclarations (scriptFile, loadScriptString + source)
                         (line + loadScript.Length, col)
       let res = [| for name, glyph, info in decls -> JsonTypes.Declaration(name, glyph, info) |]
       return! ctx |> noCacheSuccess (JsonTypes.Declarations(res).JsonValue.ToString())
@@ -327,7 +327,7 @@ let serviceHandler fsi scriptFile : WebPart = fun ctx -> async {
   | _ ->
       let webDir = Path.Combine(ctx.runtime.homeDirectory, "web")
       let subRuntime = { ctx.runtime with homeDirectory = webDir }
-      let webPart = 
+      let webPart =
         if ctx.request.url.LocalPath <> "/" then Files.browseHome
         else Files.browseFileHome "index.html"
       return! webPart { ctx with runtime = subRuntime } }
