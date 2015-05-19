109f0368752241414ba98f593c7801a8f811710f
tpetricek <tomas@tomasp.net>
2015-05-19 03:29:05 +0100

    Log more

--- app.azure.fsx
+++ app.azure.fsx
@@ -13,7 +13,7 @@ let serverConfig =
   let port = int (getBuildParam "port")
   { Web.defaultConfig with
       homeFolder = Some __SOURCE_DIRECTORY__
-      logger = Logging.Loggers.saneDefaultsFor Logging.LogLevel.Warn
+      logger = Logging.Loggers.saneDefaultsFor Logging.LogLevel.Info
       bindings = [ Types.HttpBinding.mk' Types.HTTP "127.0.0.1" port ] }
 
 Web.startWebServer serverConfig app
--- app.heroku.fsx
+++ app.heroku.fsx
@@ -2,19 +2,16 @@
 // Start the 'app' WebPart defined in 'app.fsx' on Heroku using %PORT%
 // --------------------------------------------------------------------------------------
 
-printfn "loading app"
 #load "app.fsx"
 open App
 open System
 open Suave
 
-printfn "running server"
-
 let serverConfig =
   let port = int (Environment.GetEnvironmentVariable("PORT"))
   { Web.defaultConfig with
       homeFolder = Some __SOURCE_DIRECTORY__
-      logger = Logging.Loggers.saneDefaultsFor Logging.LogLevel.Warn
+      logger = Logging.Loggers.saneDefaultsFor Logging.LogLevel.Info
       bindings = [ Types.HttpBinding.mk' Types.HTTP "0.0.0.0" port ] }
 
 Web.startWebServer serverConfig app
